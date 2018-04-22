a8d9ae0a7851c5ebc497caa4fc5a3e3ec059fecf
winerwinerchikendinner <35888927+winerwinerchikendinner@users.noreply.github.com>
2018-04-22 18:40:05 +0800

    x

--- house.py
+++ house.py
@@ -14,17 +14,29 @@ rows=data.split('\n')
 for row in rows:
     pat.append(row.split(","))
 
-mc.setBlock(-9,0,42,block.DIAMOND_BLOCK.id)
+print(pat)
+
+x0=-29
+y0=0
+z0=59
+
+mc.setBlock(x0,y0,z0,block.DIAMOND_BLOCK.id)
 for y in range(4):
     for z in range(10):
-        mc.setBlock(-9,y,z+37,block.DIAMOND_BLOCK.id)
-        mc.setBlock(-19,y,z+37,block.DIAMOND_BLOCK.id)
+        mc.setBlock(x0,y,z+z0-5,block.DIAMOND_BLOCK.id)
+        mc.setBlock(x0-10,y,z+z0-5,block.DIAMOND_BLOCK.id)
+        if z==5 and y<=1:
+            mc.setBlock(-9,y,z+z0-5,block.DOOR_WOOD.id)
+            mc.setBlock(x0-10,y,z+z0-5,block.DOOR_WOOD.id)
     for x in range(11):
-        mc.setBlock(x-19,y,47,block.DIAMOND_BLOCK.id)
-        mc.setBlock(x-19,y,37,block.DIAMOND_BLOCK.id)
+        mc.setBlock(x0-10,y,z0+5,block.DIAMOND_BLOCK.id)
+        mc.setBlock(x0-10,y,z0-5,block.DIAMOND_BLOCK.id)
+        if x==5 and y<=1:
+            mc.setBlock(x0-10,y,z0+5,block.DOOR_WOOD.id)
+            mc.setBlock(x0-10,y,z0-5,block.DOOR_WOOD.id)
 for x in range(11):
     for z in range(10):
         if pat[x][z] == "1":
-            mc.setBlock(x-19,4,z+37,block.DIAMOND_BLOCK.id)
+            mc.setBlock(x0-10,4,z+z0-5,block.DIAMOND_BLOCK.id)
         else:
-            mc.setBlock(x-19,4,z+37,block.GOLD_BLOCK.id)
+            mc.setBlock(x0-10,4,z+z0-5,block.GOLD_BLOCK.id)
--- welcomehome.py
+++ welcomehome.py
@@ -8,23 +8,12 @@ stayed_time=0
 
 #-15,0,42
 
-mc.setBlock(-9,0,42,block.DIAMOND_BLOCK.id)
-for y in range(4):
-    for z in range(10):
-        mc.setBlock(-9,y,z+37,block.DIAMOND_BLOCK.id)
-        mc.setBlock(-19,y,z+37,block.DIAMOND_BLOCK.id)
-    for x in range(11):
-        mc.setBlock(x-19,y,47,block.DIAMOND_BLOCK.id)
-        mc.setBlock(x-19,y,37,block.DIAMOND_BLOCK.id)
-for x in range(11):
-    for z in range(10):
-        mc.setBlock(x-19,4,z+37,block.DIAMOND_BLOCK.id)
 
 while True:
     time.sleep(0.5)
     pos=mc.player.getTilePos()
     mc.postToChat("please goto home x=-13--15 y=0 z=41-43 for 10s to fly")
-    mc.postToChat("x:"+str(pos.x)+"y:"+str(pos.y)+"z:"+str(pos.z)) 
+    mc.postToChat("x:"+str(pos.x)+"y:"+str(pos.y)+"z:"+str(pos.z))
     if pos.x<=-13 and  pos.x>=-15 and pos.y==-0 and pos.z>=41 and pos.z<=43:
         mc.postToChat("welcome home")
         stayed_time=stayed_time+1
@@ -33,5 +22,3 @@ while True:
             stayed_time=0
     else:
         stayed_time=0
-        
-     
