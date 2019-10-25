6f4554464e545ae211eeb526f1343fcfe580e475
dimension-zhang <56999227+dimension-zhang@users.noreply.github.com>
2019-10-25 19:47:31 +0800

    Add files via upload

--- zhang_di/zh.py
+++ zhang_di/zh.py
@@ -9,8 +9,12 @@ b=0
 x0=pos.x
 y0=pos.y
 z0=pos.z
+w0=pos.x+20
+l0=pos.y
+h0=pos.z
 
-def house (x0,y0,z0):
+
+def house (x0,y0,z0,w0,l0,h0):
     for y in range(10):
         for x in range(10):
             for z in range(10):
@@ -29,11 +33,22 @@ def house (x0,y0,z0):
     for x in range(10):
             for z in range(10):
                 mc.setBlock(pos.x+x+a,pos.y+10,pos.z+z,24,2)
+    for v in range(15):
+        for b in range(15):
+            for n in range(15):
+                mc.setBlock(w0+v,l0+b,h0+n,1)
+    for d in range(13):
+        for f in range(13):
+            for g in range(13):
+                mc.setBlock(w0+d,l0+f,h0+g,0)
+    mc.setBlock(w0+4,l0+3,h0,20)
+    mc.setBlock(w0+5,l0+3,h0,20)
+    mc.setBlock(w0+4,l0+4,h0,20)
+    mc.setBlock(w0+5,l0+4,h0,102)
+                
+                
+
+house(x0,y0,z0,w0,l0,h0)
 
 
 
-
-for b in range(1000):
-    if a <= 140:
-        house(x0+a,y0,z0)
-        a = a+14
